83a3338a3126096b1daa75f65936db429804b0dc
OpenShift guest <neallubin@gmail.com>
2014-02-03 14:38:09 -0500

    Added a demo

---
+++ javascripts/app.js
@@ -0,0 +1,14 @@
+var app = app.module("myApp", ["switcharoo"]);
+
+app.controller("MyCtrl", ['$scope', function($scope) {
+    $scope.defaultItems = [
+        1, 4
+    ];
+    $scope.listOfItems = {
+        1: 'Apples',
+        2: 'Oranges',
+        3: 'Peaches',
+        4: 'Bananas',
+        5: 'Lemons'
+    }
+}]);
